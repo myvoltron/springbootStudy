7ee56c99ff18a0a2d611625ba1b5002afd55ce9b
myvoltron <leejs8266@gmail.com>
2022-02-21 00:52:04 +0900

    스프링부트와 AWS 웹서비스 223페이지까지 완료, 스프링 시큐리티, OAuth2를 이용한 구글, 네이버 로그인 구현 인증정보는 세션을 통해 유지된다. 세션은 스프링 시큐리티가 알아서 설정해준다.

--- src/main/java/com/soloWeb/springbootStudy/web/IndexController.java
+++ src/main/java/com/soloWeb/springbootStudy/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.soloWeb.springbootStudy.web;
 
+import com.soloWeb.springbootStudy.config.auth.LoginUser;
+import com.soloWeb.springbootStudy.config.auth.dto.SessionUser;
 import com.soloWeb.springbootStudy.service.posts.PostsService;
 import com.soloWeb.springbootStudy.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,10 +19,13 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
 
         model.addAttribute("posts", postsService.findAllDesc());
 
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
